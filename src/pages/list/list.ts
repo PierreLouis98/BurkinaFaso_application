986d4fd9b795c607e2f6f799a2918e0f70df7c4c
PierreLouis98 <pierre-louis.de-soultrait@edu.ece.fr>
2018-08-09 12:28:22 +0000

    Simplification du code list.ts

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -10,57 +10,74 @@ import { ItemDetailsPage } from '../item-details/item-details';
 })
 
 
-export class ListPage {
+export class ListPage 
+{
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-
-/**
-*   
-*
-*/
   constructor(public navCtrl: NavController, public navParams: NavParams) 
-  {
-  
-  /** Tableau d'icon */
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+      {
 
-  /** 
-  * Tableau d'item (ensemble des informations sur un bouton)
-  */
-    this.items = [];
+      /** Tableau d'icon */
+        this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+        'american-football', 'boat', 'bluetooth', 'build'];
 
-     
-     
-     this.items.push({ title: 'Etape ' + 1, note: 'La préparation' });
-     this.items.push({ title: 'Etape ' + 2, note: 'La semence' });
-     this.items.push({ title: 'Etape ' + 3, note: 'Le semis' });
-     this.items.push({ title: 'Etape ' + 4, note: 'Le sarclage' });
-     this.items.push({ title: 'Etape ' + 5, note: 'Les engrais' });
-     this.items.push({ title: 'Etape ' + 6, note: 'Les traitements' });
-     this.items.push({ title: 'Etape ' + 7, note: 'La récolte'  });
-     this.items.push({ title: 'Etape ' + 8, note: 'Le séchage' });
-     this.items.push({ title: 'Etape ' + 9, note: 'Le battage' });
-     this.items.push({ title: 'Etape ' + 10, note: 'Le tri' });
-     this.items.push({ title: 'Etape ' + 11, note: 'La conservation' });
-     this.items.push({ title: 'Etape ' + 12, note: 'Le stockage' });
-     
-     /**   this.items.push({ title: 'Etape ' + 3, note: 'Le semis', icon: this.icons[Math.floor(Math.random() * this.icons.length)] }); 
-     */
-  }
+     /** Tableau de notes */
+        this.note = ['La préparation', 'La semence', 'Le semis', 'Le sarclage', 'Les engrais', 
+        'Les traitements', 'La récolte', 'Le séchage', 'Le battage', 'Le tri', 'La conservation', 
+        'Le stockage'];
+
+      /** Tableau de titre */
+        this.title = ['Etape '];
+
+      /** Tableau d'item contenant le tableau d'icon, de note et de titre */
+        this.items = [];
 
 
+        for (var i = 0; i < 12; i++)
+            this.items.push({ title: this.title[0] + (i+1), note: this.note[i] , icon: this.icons[4] }); 
+
+      }
 
 
-
-/**
- *  Le .push permet d'ouvrir la nouvelle page
- *  On passe en paramètre le logo qui est déjà visible sur le bouton
- *  navCtrl pour naviguer. Plus d'infos sur : *https://ionicframework.com/docs/3.5.0/api/navigation/NavController/
- */
- 
-  itemTapped(event, item) {
-    this.navCtrl.push(ItemDetailsPage, {item: item});
-  }
+        /**
+         *  Le .push permet d'ouvrir la nouvelle page
+         *  On passe en paramètre toutes les infos qui étaient déjà visible sur le bouton
+         *  navCtrl pour naviguer. Plus d'infos sur : *https://ionicframework.com/docs/3.5.0/api/navigation/NavController/
+         */
+          itemTapped(event, item) {
+            this.navCtrl.push(ItemDetailsPage, {item: item});
+     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
