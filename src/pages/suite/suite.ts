cab62a1b0239f6a7028e665d9e22060443516672
PierreLouis98 <pierre-louis.de-soultrait@edu.ece.fr>
2018-08-10 18:44:08 +0000

    Ajout de radio dans la simulation (séléction d'une seule réponse)

--- src/pages/suite/suite.ts
+++ src/pages/suite/suite.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+
+import {
+  FormGroup,
+  FormControl
+
+} from '@angular/forms';
+
 /**
  * Generated class for the SuitePage page.
  *
@@ -15,16 +22,25 @@ import { NavController, NavParams } from 'ionic-angular';
 export class SuitePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   this.langForm = new FormGroup({
+      "langs": new FormControl({value: 'rust', disabled: false})
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SuitePage');
   }
   
-  score(p1, p2) {
+ score(p1, p2) {
     var resultat = p1 + p2;
     if((p1 === 1 || p1 === 0) && (p2 === 1 || p2 === 0))
         return(resultat + ' pts');
+  }
+  
+  
+  doSubmit(event) {
+    console.log('Submitting form', this.langForm.value);
+    event.preventDefault();
   }
 
 }
